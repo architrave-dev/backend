a225fe573ef139d6c0eb1c04ef9c2445fc7ed38e
moveuk <v_donguk@naver.com>
2024-06-24 19:59:39 +0900

    set spring java based project

---
+++ src/main/java/com/architrave/portfolio/ArchitraveApplication.java
@@ -0,0 +1,13 @@
+package com.architrave.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitraveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitraveApplication.class, args);
+    }
+
+}
