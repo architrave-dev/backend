7971fbb2584c73425b4836b0113ed47731ce219b
youngdong82 <nfs82young@gmail.com>
2024-06-28 08:05:44 +0900

    Fix: change @Data Entity to others
    @Data 를
    @Getter, @NoArgsConstructor, @AllArgsConstructor로 변경

--- src/main/java/com/architrave/portfolio/domain/model/UploadFile.java
+++ src/main/java/com/architrave/portfolio/domain/model/UploadFile.java
@@ -5,12 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UploadFile extends BaseEntity {
 
     @Id @GeneratedValue
