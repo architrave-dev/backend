71c0b0fc5cc90e8468a911c132e91a5a4c617649
youngdong82 <nfs82young@gmail.com>
2024-07-02 23:55:00 +0900

    feat: set required value @NotNull in Member Entity
    email, password, role, aui @NotNull 설정

--- src/main/java/com/architrave/portfolio/domain/model/Member.java
+++ src/main/java/com/architrave/portfolio/domain/model/Member.java
@@ -2,6 +2,7 @@ package com.architrave.portfolio.domain.model;
 
 import com.architrave.portfolio.domain.model.enumType.RoleType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,12 +19,20 @@ public class Member extends BaseEntity{
     @Column(name = "member_id")
     private Long id;
 
+    @Column(unique = true)
+    @NotNull
     private String email;
+
+    @NotNull
     private String password;
+    @Column(unique = true)
+    @NotNull
     private String aui;
 
     @Enumerated
+    @NotNull
     private RoleType role;
+
     private String description;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
