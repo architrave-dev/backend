4d94c99ed21938de1e03df7878724781ed48f34f
youngdong82 <nfs82young@gmail.com>
2024-07-03 21:03:55 +0900

    feat: remove @Builder in Member & create custom MemberBuilder
    누락된 활동명을 뜻하는 username 추가 (중복가능)
    username에서 파생되는 AUI(중복불가) 생성 메소드 추가
    username 설정 시 자동으로 AUI 생성 및 설정 등을 위해 커스텀 Builder class 생성

---
+++ src/main/java/com/architrave/portfolio/domain/model/builder/MemberBuilder.java
@@ -0,0 +1,61 @@
+package com.architrave.portfolio.domain.model.builder;
+
+import com.architrave.portfolio.domain.model.LandingBox;
+import com.architrave.portfolio.domain.model.Member;
+import com.architrave.portfolio.domain.model.enumType.RoleType;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MemberBuilder {
+    private String email;
+    private String password;
+    private String username;
+    private RoleType role;
+    private String description;
+    private LandingBox loadingBox;
+
+    public MemberBuilder email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public MemberBuilder password(String password) {
+        this.password = password;
+        return this;
+    }
+    public MemberBuilder username(String username) {
+        this.username = username;
+        return this;
+    }
+    public MemberBuilder role(RoleType role) {
+        this.role = role;
+        return this;
+    }
+
+    public MemberBuilder description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public MemberBuilder loadingBox(LandingBox loadingBox) {
+        this.loadingBox = loadingBox;
+        return this;
+    }
+    public Member build() {
+        validateMember();
+        return Member.createMember(
+                this.email,
+                this.password,
+                this.username,
+                this.role,
+                this.description,
+                this.loadingBox
+        );
+    }
+    private void validateMember(){
+        if(email == null || password == null || username == null || role == null){
+            throw new IllegalArgumentException("required value is empty");
+        }
+    }
+
+}
