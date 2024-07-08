dfb9ec769cdb20597b15b2614c9cacd7ee19e6c2
youngdong82 <nfs82young@gmail.com>
2024-07-08 12:50:46 +0900

    debug : create constructor for init dto

--- src/main/java/com/architrave/portfolio/api/dto/auth/request/CreateMemberReq.java
+++ src/main/java/com/architrave/portfolio/api/dto/auth/request/CreateMemberReq.java
@@ -3,8 +3,10 @@ package com.architrave.portfolio.api.dto.auth.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateMemberReq{
     private String email;
