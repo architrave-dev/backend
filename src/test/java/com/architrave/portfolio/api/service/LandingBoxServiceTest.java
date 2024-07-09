65e0e9c18fc00193b5b8bf58230bbb697850708f
youngdong82 <nfs82young@gmail.com>
2024-07-10 00:16:48 +0900

    test: create test list about landing box

---
+++ src/test/java/com/architrave/portfolio/api/service/LandingBoxServiceTest.java
@@ -0,0 +1,30 @@
+package com.architrave.portfolio.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LandingBoxServiceTest {
+
+    @Test
+    public void createLandingBox(){}
+
+    @Test
+    public void EmptyImgWhenCreateLandingBox(){}
+
+    @Test
+    public void WrongMemberWhenCreateLandingBox(){}
+
+    @Test
+    public void EmptyImgWhenUpdateLandingBox(){}
+
+    @Test
+    public void updateLandingBox(){}
+
+    @Test
+    public void WrongMemberWhenUpdateLandingBox(){}
+
+    @Test
+    public void removeLandingBox(){}
+
+}
