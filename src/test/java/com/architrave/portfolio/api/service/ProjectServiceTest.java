645d1d87977fada5a39472f34b17c44fb68088e0
youngdong82 <nfs82young@gmail.com>
2024-07-13 20:49:48 +0900

    teat: create test list about Project logic

---
+++ src/test/java/com/architrave/portfolio/api/service/ProjectServiceTest.java
@@ -0,0 +1,29 @@
+package com.architrave.portfolio.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProjectServiceTest {
+    @Test
+    public void getProjectList(){}
+
+    @Test
+    public void getProjectListEmpty(){}
+
+    @Test
+    public void getProjectDetail(){}
+
+    @Test
+    public void createProject(){}
+
+    @Test
+    public void AccessDeniedWhenCreateProject(){}
+
+    @Test
+    public void updateProject(){}
+
+    @Test
+    public void AccessDeniedWhenUpdateProject(){}
+
+}
