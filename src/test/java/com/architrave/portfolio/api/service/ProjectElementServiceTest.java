0a513652b0c0b42e938cfdf87f7c3e7dde6ba304
youngdong82 <nfs82young@gmail.com>
2024-07-17 12:44:06 +0900

    test: create test list about projectElement

---
+++ src/test/java/com/architrave/portfolio/api/service/ProjectElementServiceTest.java
@@ -0,0 +1,57 @@
+package com.architrave.portfolio.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class ProjectElementServiceTest {
+
+    @Test
+    @Transactional
+    public void getProjectElementsInProject(){}
+
+    @Test
+    @Transactional
+    public void addWorkInProject(){}
+
+    @Test
+    @Transactional
+    public void addTextBoxInProject(){}
+
+    @Test
+    @Transactional
+    public void addDividerInProject(){}
+
+    @Test
+    @Transactional
+    public void updateWorkInProject(){}
+
+    @Test
+    @Transactional
+    public void updateTextBoxInProject(){}
+
+    @Test
+    @Transactional
+    public void updateDividerInProject(){}
+
+    @Test
+    @Transactional
+    public void removeWorkInProject(){}
+
+    @Test
+    @Transactional
+    public void removeTextBoxInProject(){}
+
+    @Test
+    @Transactional
+    public void removeDividerInProject(){}
+
+    @Test
+    @Transactional
+    public void changeOrderInProject(){}
+
+    @Test
+    @Transactional
+    public void changeRepresentitiveInProject(){}
+}
