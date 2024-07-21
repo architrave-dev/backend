d2f087130bce168bd36974555379020bdd7dcfac
youngdong82 <nfs82young@gmail.com>
2024-07-21 17:35:03 +0900

    feat: create ProjectElement repository, service

---
+++ src/main/java/com/architrave/portfolio/api/service/ProjectElementService.java
@@ -0,0 +1,135 @@
+package com.architrave.portfolio.api.service;
+
+import com.architrave.portfolio.domain.model.Project;
+import com.architrave.portfolio.domain.model.ProjectElement;
+import com.architrave.portfolio.domain.model.TextBox;
+import com.architrave.portfolio.domain.model.Work;
+import com.architrave.portfolio.domain.model.enumType.DividerType;
+import com.architrave.portfolio.domain.model.enumType.TextBoxAlignment;
+import com.architrave.portfolio.domain.model.enumType.WorkAlignment;
+import com.architrave.portfolio.domain.repository.ProjectElementRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectElementService {
+
+    private final ProjectElementRepository projectElementRepository;
+
+
+    @Transactional
+    public ProjectElement createProjectElement(ProjectElement projectElement) {
+        return projectElementRepository.save(projectElement);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProjectElement> findProjectElementByProject(Project project) {
+        return projectElementRepository.findByProject(project);
+    }
+
+    @Transactional(readOnly = true)
+    public ProjectElement findById(Long id) {
+        return projectElementRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("there is no project element that id: " + id));
+    }
+    @Transactional
+    public ProjectElement updateProjectElementWork(Work work,
+                                                   Long projectElementId,
+                                                   WorkAlignment workAlignment,
+                                                   Integer peOrder,
+                                                   Boolean isRepresentative) {
+        ProjectElement projectElement = findById(projectElementId);
+        //work 내 변경사항은 이미 완료한 상태
+        if(!projectElement.getWork().equals(work)){
+           projectElement.setWork(work);
+        }
+        if(workAlignment != null){
+            projectElement.setWorkAlignment(workAlignment);
+        }
+        if(peOrder != null){
+            projectElement.setPeOrder(peOrder);
+        }
+        if(isRepresentative != null){
+            projectElement.setIsRepresentative(isRepresentative);
+        }
+        return projectElement;
+    }
+
+    @Transactional
+    public ProjectElement updateProjectElementTextBox(TextBox textBox,
+                                                      Long projectElementId,
+                                                      TextBoxAlignment textBoxAlignment,
+                                                      Integer peOrder) {
+        ProjectElement projectElement = findById(projectElementId);
+        //textBox 내 변경사항은 이미 완료한 상태
+        if(!projectElement.getTextBox().equals(textBox)){
+            projectElement.setTextBox(textBox);
+        }
+        if(textBoxAlignment != null){
+            projectElement.setTextBoxAlignment(textBoxAlignment);
+        }
+        if(peOrder != null){
+            projectElement.setPeOrder(peOrder);
+        }
+        return projectElement;
+    }
+
+    @Transactional
+    public ProjectElement updateProjectElementDivider(Long projectElementId,
+                                                      DividerType dividerType,
+                                                      Integer peOrder) {
+        ProjectElement projectElement = findById(projectElementId);
+        if(dividerType != null){
+            projectElement.setDividerType(dividerType);
+        }
+        if(peOrder != null){
+            projectElement.setPeOrder(peOrder);
+        }
+        return projectElement;
+    }
+
+    @Transactional
+    public void removeById(Long id) {
+        ProjectElement projectElement = findById(id);
+        projectElementRepository.delete(projectElement);
+    }
+
+    @Transactional
+    public void organizeOrder(Project project){
+        List<ProjectElement> projectElementList = findProjectElementByProject(project);
+        for(int i=0; i<projectElementList.size(); i++){
+            ProjectElement projectElement = projectElementList.get(i);
+            projectElement.setPeOrder(i+1);
+        }
+    }
+
+
+//    @Transactional
+//    public ProjectElement updateWithHandler(Long id,
+//                                            Long projectId,
+//                                            Work work,
+//                                            WorkAlignment workAlignment,
+//                                            TextBox textBox,
+//                                            TextBoxAlignment textBoxAlignment,
+//                                            DividerType dividerType,
+//                                            Integer peOrder,
+//                                            Boolean isRepresentative
+//    ){
+//        if(work != null){    //work라면
+//            return updateProjectElementWork(work, id, workAlignment, peOrder,
+//                                            isRepresentative);
+//
+//        }else if(textBox != null) {  //textBox라면
+//            return updateProjectElementTextBox(textBox, id, textBoxAlignment, peOrder);
+//
+//        }else{  //divider 라면
+//            return updateProjectElementDivider(id, dividerType, peOrder);
+//        }
+//    }
+
+}
