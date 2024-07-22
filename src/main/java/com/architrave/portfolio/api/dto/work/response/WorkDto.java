5e6f1ec6e72c11be0f5fabaeb3252f24513c9e62
youngdong82 <nfs82young@gmail.com>
2024-07-22 12:48:47 +0900

    fix: convert Dto about work to less dependency

--- src/main/java/com/architrave/portfolio/api/dto/work/response/WorkDto.java
+++ src/main/java/com/architrave/portfolio/api/dto/work/response/WorkDto.java
@@ -4,29 +4,32 @@ import com.architrave.portfolio.domain.model.Member;
 import com.architrave.portfolio.domain.model.Size;
 import com.architrave.portfolio.domain.model.UploadFile;
 import com.architrave.portfolio.domain.model.Work;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-public class WorkResponse {
+public class WorkDto {
     private Long id;
     private Member member;
-    private UploadFile uploadFile;
+    private String originImgUrl;
+    private String thumbnailUrl;
     private String title;
     private String description;
     private Size size;
     private String material;
     private Integer prodYear;
+    private Boolean isDeleted;
 
-    @Builder
-    public WorkResponse(Work work) {
+    public WorkDto(Work work) {
         this.id = work.getId();
         this.member = work.getMember();
-        this.uploadFile = work.getUploadFile();
+        UploadFile uploadFile = work.getUploadFile();
+        this.originImgUrl = uploadFile.getOriginUrl();
+        this.thumbnailUrl = uploadFile.getThumbnailUrl();
         this.title = work.getTitle();
         this.description = work.getDescription();
         this.size = work.getSize();
         this.material = work.getMaterial();
         this.prodYear = work.getProdYear();
+        this.isDeleted = work.getIsDeleted();
     }
 }
