091fc2ea7c1f8a05c036e563c09c899d05707436
youngdong82 <nfs82young@gmail.com>
2024-07-24 19:52:12 +0900

    chore: Swagger setting-3

--- src/main/java/com/architrave/portfolio/api/controller/ProjectController.java
+++ src/main/java/com/architrave/portfolio/api/controller/ProjectController.java
@@ -6,10 +6,7 @@ import com.architrave.portfolio.api.dto.project.response.ProjectDto;
 import com.architrave.portfolio.api.dto.project.response.ProjectInfoDto;
 import com.architrave.portfolio.api.dto.project.response.ProjectSimpleDto;
 import com.architrave.portfolio.api.dto.projectElement.response.ProjectElementDto;
-import com.architrave.portfolio.api.service.AuthService;
-import com.architrave.portfolio.api.service.MemberService;
-import com.architrave.portfolio.api.service.ProjectInfoService;
-import com.architrave.portfolio.api.service.ProjectService;
+import com.architrave.portfolio.api.service.*;
 import com.architrave.portfolio.domain.model.Member;
 import com.architrave.portfolio.domain.model.Project;
 import com.architrave.portfolio.domain.model.ProjectInfo;
@@ -82,7 +79,7 @@ public class ProjectController {
 
     @Operation(
             summary = "Project 생성하기",
-            description = "간단하게 title, description, 대표이미지 만으로 생성합니다. <br />" +
+            description = "간단하게 title, description, UploadFile 만으로 생성합니다. <br />" +
                     "이후 Project 내의 세부사항은 update 요청으로 진행합니다."
     )
     @PostMapping
@@ -115,7 +112,15 @@ public class ProjectController {
      * @param updateProjectReq
      * @return
      */
-    @Operation(summary = "Project 수정하기")
+    @Operation(
+            summary = "Project 수정하기",
+            description = "projectInfo 관련 설명 <br/><br/>" +
+                    "Project 내에 ProjectInfo 생성, 수정, 삭제를 위해 3가지로 나뉩니다. <br />" +
+                    "createdProjectInfoList: 생성된 ProjectInfo <br/>" +
+                    "updatedProjectInfoList: 생성된 ProjectInfo <br/>" +
+                    "removedProjectInfoList: 삭제된 ProjectInfo <br/><br/>" +
+                    "Project 삭제 시 isDeleted 를 true 로 변경합니다."
+    )
     @PutMapping
     public ResponseEntity<ResultDto<ProjectDto>> updateProject(
             @RequestParam("aui") String aui,
