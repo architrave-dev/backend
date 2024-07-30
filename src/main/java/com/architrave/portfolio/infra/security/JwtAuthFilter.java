bc48c0be649af702039134d3890b113365fed4f2
youngdong82 <nfs82young@gmail.com>
2024-07-30 11:18:58 +0900

    fix: convert hear name Authentication to Authorization

--- src/main/java/com/architrave/portfolio/infra/security/JwtAuthFilter.java
+++ src/main/java/com/architrave/portfolio/infra/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,11 +27,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authentication");
+        String authHeader = request.getHeader("Authorization");
         String email;
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
