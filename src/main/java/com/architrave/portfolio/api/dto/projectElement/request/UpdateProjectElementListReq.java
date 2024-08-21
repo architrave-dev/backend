622f24978e2d3527e8cb30ac9fdadaba2006349d
youngdong82 <nfs82young@gmail.com>
2024-08-22 08:48:47 +0900

    feat: create new dto for update ProjectElementList

--- src/main/java/com/architrave/portfolio/api/dto/projectElement/request/UpdateProjectElementListReq.java
+++ src/main/java/com/architrave/portfolio/api/dto/projectElement/request/UpdateProjectElementListReq.java
@@ -1,2 +1,21 @@
-package com.architrave.portfolio.api.dto.projectElement.request;public class UpdateProjectElementListReq {
+package com.architrave.portfolio.api.dto.projectElement.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProjectElementListReq {
+
+    @NotNull
+    private Long projectId;
+    private List<CreateProjectElementReq> createProjectElements;
+    private List<UpdateProjectElementReq> updatedProjectElements;
+    private List<RemoveProjectElementReq> removedProjectElements;
+
 }
--- src/main/java/com/architrave/portfolio/api/dto/projectElement/request/UpdateProjectElementReq.java
+++ src/main/java/com/architrave/portfolio/api/dto/projectElement/request/UpdateProjectElementReq.java
@@ -1,5 +1,7 @@
 package com.architrave.portfolio.api.dto.projectElement.request;
 
+import com.architrave.portfolio.api.dto.textBox.request.UpdateTextBoxReq;
+import com.architrave.portfolio.api.dto.work.request.UpdateWorkReq;
 import com.architrave.portfolio.domain.model.enumType.DividerType;
 import com.architrave.portfolio.domain.model.enumType.TextBoxAlignment;
 import com.architrave.portfolio.domain.model.enumType.WorkAlignment;
@@ -12,10 +14,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UpdateProjectElementReq {
     private Long id;
-    private Long projectId;
-    private Long workId;
+    private UpdateWorkReq updateWorkReq;
     private WorkAlignment workAlignment;
-    private Long textBoxId;
+    private UpdateTextBoxReq updateTextBoxReq;
     private TextBoxAlignment textBoxAlignment;
     private DividerType dividerType;
     private Integer peOrder;
