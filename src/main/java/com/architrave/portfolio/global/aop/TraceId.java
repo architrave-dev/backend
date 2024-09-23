6f00b92bfe45614a59895deda1476b011f000979
youngdong82 <nfs82young@gmail.com>
2024-09-24 08:32:47 +0900

    feat: create TraceId, TraceStatus

---
+++ src/main/java/com/architrave/portfolio/global/aop/TraceId.java
@@ -0,0 +1,35 @@
+package com.architrave.portfolio.global.aop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class TraceId {
+    private String id;
+    private int depth;
+
+
+    public TraceId(){
+        this.id = createId();
+        this.depth = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    public TraceId createNextTraceId(){
+        return new TraceId(id, depth +1);
+    }
+
+    public TraceId createPrevTraceId(){
+        return new TraceId(id, depth -1);
+    }
+
+    public boolean isFirstDepth(){
+        return depth == 0;
+    }
+}
