bdf3f9efe8afbe61594c7b1533e74f3c6dbe2940
youngdong82 <nfs82young@gmail.com>
2024-11-11 21:07:10 +0900

    remove: remove previous test code.

--- src/test/java/com/architrave/portfolio/ArchitraveApplicationTests.java
+++ src/test/java/com/architrave/portfolio/ArchitraveApplicationTests.java
@@ -1,13 +1,7 @@
 package com.architrave.portfolio;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ArchitraveApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
