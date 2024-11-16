8f1896e56bcf54dd5a290fa1302a94d65c2bc150
youngdong82 <nfs82young@gmail.com>
2024-11-17 02:46:29 +0900

    feat: add DOCUMENT in ProjectElementType

--- src/main/java/com/architrave/portfolio/api/dto/projectElement/response/ProjectElementDto.java
+++ src/main/java/com/architrave/portfolio/api/dto/projectElement/response/ProjectElementDto.java
@@ -1,6 +1,7 @@
 package com.architrave.portfolio.api.dto.projectElement.response;
 
 import com.architrave.portfolio.api.dto.work.response.WorkDto;
+import com.architrave.portfolio.domain.model.Document;
 import com.architrave.portfolio.domain.model.ProjectElement;
 import com.architrave.portfolio.domain.model.TextBox;
 import com.architrave.portfolio.domain.model.enumType.*;
@@ -17,6 +18,8 @@ public class ProjectElementDto {
     private WorkDisplaySize workDisplaySize;
     private TextBox textBox;
     private TextBoxAlignment textBoxAlignment;
+    private Document document;
+    private WorkAlignment documentAlignment;
     private DividerType dividerType;
 
     public ProjectElementDto(ProjectElement projectElement){
@@ -29,12 +32,17 @@ public class ProjectElementDto {
         //textbox
         this.textBox = projectElement.getTextBox();
         this.textBoxAlignment = projectElement.getTextBoxAlignment();
+        //document
+        this.document = projectElement.getDocument();
+        this.documentAlignment = projectElement.getDocumentAlignment();
         //divider
         this.dividerType = projectElement.getDividerType();
         if(this.work != null){
             this.projectElementType = ProjectElementType.WORK;
         }else if(this.textBox != null){
             this.projectElementType = ProjectElementType.TEXTBOX;
+        }else if(this.document != null){
+            this.projectElementType = ProjectElementType.DOCUMENT;
         }else if(this.dividerType != null){
             this.projectElementType = ProjectElementType.DIVIDER;
         }
