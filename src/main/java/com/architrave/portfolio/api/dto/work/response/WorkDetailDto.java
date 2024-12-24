1d584707e80b4679a89d3ee6cd3dd6e342dce6ca
youngdong82 <nfs82young@gmail.com>
2024-12-24 17:15:26 +0900

    remove: remove workType property in WorkDetail -2

--- src/main/java/com/architrave/portfolio/api/dto/work/response/WorkDetailDto.java
+++ src/main/java/com/architrave/portfolio/api/dto/work/response/WorkDetailDto.java
@@ -2,19 +2,16 @@ package com.architrave.portfolio.api.dto.work.response;
 
 import com.architrave.portfolio.domain.model.UploadFile;
 import com.architrave.portfolio.domain.model.WorkDetail;
-import com.architrave.portfolio.domain.model.enumType.WorkType;
 import lombok.Data;
 
 @Data
 public class WorkDetailDto {
     private Long id;
-    private WorkType workType;
     private UploadFile uploadFile;
     private String description;
 
     public WorkDetailDto(WorkDetail workDetail) {
         this.id = workDetail.getId();
-        this.workType = workDetail.getWorkType();
         this.uploadFile = workDetail.getUploadFile();
         this.description = workDetail.getDescription();
     }
