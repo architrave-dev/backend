98922b075c685ab2fb52751dd0a92277fc9049ec
youngdong82 <nfs82young@gmail.com>
2025-03-07 13:19:23 +0900

    feat: add new ErrorCode and handler

--- src/main/java/com/architrave/portfolio/global/exception/custom/NoMatchEnumException.java
+++ src/main/java/com/architrave/portfolio/global/exception/custom/NoMatchEnumException.java
@@ -1,15 +1,14 @@
 package com.architrave.portfolio.global.exception.custom;
 
 import lombok.Getter;
-import org.springframework.security.core.AuthenticationException;
 
 @Getter
-public class InvalidTokenException extends AuthenticationException {
-    public InvalidTokenException(String msg, Throwable cause) {
+public class NoMatchEnumException extends IllegalArgumentException {
+    public NoMatchEnumException(String msg, Throwable cause) {
         super(msg, cause);
     }
 
-    public InvalidTokenException(String msg) {
+    public NoMatchEnumException(String msg) {
         super(msg);
     }
 }
